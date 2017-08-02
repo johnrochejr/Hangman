d6f480a0084b387b16b29a1b1411eddf68bc3755
John Roche <roche.johnjr@gmail.com>
2017-08-02 15:00:41 -0400

    now replacing underscores with correct

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const session = require('express-session');
 
 const app = express();
 
-// sessions
 app.use (
   session ({
     secret: 'tame impala',
@@ -24,28 +23,44 @@ app.set('view engine', 'mst');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-
-// Step 1: Below, we took the 'words' variable (array) which constitutes the dictionary
-// randomWord is a variable that is comprised of a random selection of
-// ... words.length (a random [index] will be pulled)
-// We then place the random [index] into our 'words' array to pull a random
-// [index] which will emerge in the form of a random word
 const randomWord = (Math.floor(Math.random() * words.length));
 const wordForGame = () => {
   return words[randomWord];
-}
+};
+
+app.post('/guess', (req, res) => {
+  gameData = req.session;
+  gameData.guess = req.body.guess;
+  for (var i = 0; i < gameData.underScores.length; i++) {
+    if (gameData.actualWord[i] === gameData.guess) {
+      gameData.underScores[i] = gameData.guess
+    }
+  }
+  console.log(gameData.guess);
+  res.render('home', gameData);
+});
+
+app.get('/', (req, res) => {
+  gameData = req.session
+  gameData.actualWord = wordForGame()
+  console.log(gameData.actualWord)
+  gameData.underScores = gameData.actualWord.split("").map(letter => {
+    return letter = "_"
+  })
+  res.render('home', gameData);
+});
+
+app.listen(3000, () => {
+  console.log('Port 3000 we are go for launch!');
+});
+
+
 // console.log(wordForGame);
 // console.log(Math.floor(Math.random() * words.length));
 // console.log(wordForGame);
 
 // make an array that creates _s for as many letters that
 // our random word is
-
-let underScores = [];
-
-for (let i = 0; i < wordForGame.length; i++) {
-  underScores.push('_');
-}
 
 // test forLoop
 // console.log(underScores);
@@ -61,20 +76,3 @@ for (let i = 0; i < wordForGame.length; i++) {
 
 // How do I accept input (guess) as a variable from form,
 // and store 'guess' in session?
-app.post('/guess', (req, res) => {
-  gameData = req.session;
-  gameData.guess = req.body.guess || [];
-  console.log(gameData.guess);
-  res.render('home', gameData);
-  // not sure this is right
-})
-
-app.get('/', (req, res) => {
-  gameData = req.session
-  gameData.actualWord = wordForGame()
-  res.render('home', { underScores });
-});
-
-app.listen(3000, () => {
-  console.log('Port 3000 we are go for launch!');
-});
